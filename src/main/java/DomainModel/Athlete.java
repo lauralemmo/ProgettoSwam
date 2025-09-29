71fbf7eb1e82b6aba46cad5d16002e5a5acbc30c
Laura Lemmo <laura.lemmo@stud.unifi.it>
2025-09-29 13:28:45 +0200

    modificata classe Atleta

--- src/main/java/DomainModel/Athlete.java
+++ src/main/java/DomainModel/Athlete.java
@@ -1,6 +1,21 @@
 package DomainModel;
 
+import java.util.ArrayList;
+
 public class Athlete {
     private String height;
     private String weight;
+    private User user;
+    private Subscription subscription;
+    private int workoutPlanId;
+    private ArrayList<Booking> bookings;
+
+    public Athlete(String height, String weight, User user, int workoutPlanId) {
+        this.height = height;
+        this.weight = weight;
+        this.user = user;
+        this.subscription = new Subscription();
+        this.workoutPlanId = workoutPlanId;
+        this.bookings = new ArrayList<>();
+    }
 }
