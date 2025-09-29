294d6b34648cd8df7f82c732dd3799ea15eac033
scappugi <simone.cappugi@stud.unifi.it>
2025-09-29 15:34:36 +0200

    metodi get and set di atleta e user

--- src/main/java/DomainModel/Athlete.java
+++ src/main/java/DomainModel/Athlete.java
@@ -18,4 +18,52 @@ public class Athlete {
         this.workoutPlanId = workoutPlanId;
         this.bookings = new ArrayList<>();
     }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Subscription getSubscription() {
+        return subscription;
+    }
+
+    public void setSubscription(Subscription subscription) {
+        this.subscription = subscription;
+    }
+
+    public int getWorkoutPlanId() {
+        return workoutPlanId;
+    }
+
+    public void setWorkoutPlanId(int workoutPlanId) {
+        this.workoutPlanId = workoutPlanId;
+    }
+
+    public ArrayList<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(ArrayList<Booking> bookings) {
+        this.bookings = bookings;
+    }
 }
