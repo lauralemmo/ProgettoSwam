a5c64d0d07a4a4f286c948d9fb870a9d616446a8
Laura Lemmo <laura.lemmo@stud.unifi.it>
2025-09-29 11:40:10 +0200

    first commit

---
+++ src/main/java/org/example/progettoswam_cappugilemmo/HelloApplication.java
@@ -0,0 +1,9 @@
+package org.example.progettoswam_cappugilemmo;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
