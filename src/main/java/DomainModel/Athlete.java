471768bd5c7de716562d3cdc5c0ab3411061b0d7
scappugi <simone.cappugi@stud.unifi.it>
2025-09-29 12:02:42 +0200

    eliminato codice casuale

--- src/main/java/DomainModel/Athlete.java
+++ src/main/java/DomainModel/Athlete.java
@@ -1,47 +1,5 @@
 package DomainModel;
 
 public class Athlete {
-    private String name;
-    private String country;
-    private int age;
-    private String sport;
 
-    public Athlete(String name, String country, int age, String sport){
-        this.name = name;
-        this.country = country;
-        this.age = age;
-        this.sport = sport;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getSport() {
-        return sport;
-    }
-
-    public void setSport(String sport) {
-        this.sport = sport;
-    }
 }
