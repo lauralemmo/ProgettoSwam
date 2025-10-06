28110d1be8c6ebfaca7ad3d3f3b08f2ae78488a9
scappugi <simone.cappugi@stud.unifi.it>
2025-10-06 12:14:16 +0200

    Eliminato il Builder per gli esercizi

--- src/main/java/DomainModel/PersonalTrainer.java
+++ src/main/java/DomainModel/PersonalTrainer.java
@@ -6,15 +6,17 @@ public class PersonalTrainer {
     private int salary;
     WorkoutPlanBuilder workoutPlanBuilder;
     private User user;
+    WorkoutPlan workoutPlan;
 
     public PersonalTrainer(int salary, WorkoutPlanBuilder workoutPlanBuilder, User user) {
         this.user = user;
         this.salary = salary;
         this.workoutPlanBuilder = workoutPlanBuilder;
     }
-    public PersonalTrainer(int salary, User user) {
+    public PersonalTrainer(int salary, User user, WorkoutPlan workoutPlan) {
         this.user = user;
         this.salary = salary;
+        this.workoutPlan = workoutPlan;
     }
 
     public int getSalary() {
